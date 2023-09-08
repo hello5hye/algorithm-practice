716153f04eecd22f90abc1ceafc12d414495799e
hello5hye <hello.hyeseon@gmail.com>
2023-09-08 20:40:33 +0900

    training1-16 add

--- src/main/java/training1/training1_16.java
+++ src/main/java/training1/training1_16.java
@@ -3,22 +3,24 @@ package training1;
 
 import java.util.Scanner;
 
-// 2자리의 양수(10~99)를 입력
+// 이중 루프로 구구단 곱셉표 출력
 public class training1_16 {
     public static void main(String[] args) {
-        Scanner stdIn =new Scanner(System.in);
+        System.out.println("--------구구단 곱셈표--------");
 
-        int no;
-        System.out.println("2자리의 양수를 입력하세요");
-
-        do {
-            System.out.print("no 값 입력 : ");
-            no = stdIn.nextInt();
-        } while (no < 10 || no > 99);
-
-        System.out.println("변수 no값은 " + no + "이 되었습니다.");
+        System.out.print("     |");
+        for(int i = 1; i <= 9; i++)
+            System.out.printf("%3d", i);
+        System.out.println("\n--------------------");
 
 
 
+
+        for(int i = 1; i <= 9; i++)  {
+            for (int j=1; j <= 9; j++)
+                System.out.printf("%3d", i * j);
+            System.out.println();
+
+        }
     }
 }
